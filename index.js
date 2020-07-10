bfde9151ba46ec72f39e73f9a898290322943465
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-10 15:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ class tree {
     return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves"
   }
 }
+class Deciduous extend tree {
+  constructor(species, name) {
+    this.species = species
+    this.name = name
+  }
+}
